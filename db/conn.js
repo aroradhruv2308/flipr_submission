73fa9e5cc65ea6df82f22164dad888e994853c3a
Dhruv Arora <aroradhruv2308@gmail.com>
2021-10-15 16:57:15 +0530

    initializing setup and connected mongoDB

---
+++ db/conn.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require("mongodb");
+const Db = "mongodb+srv://backendconcoxdeveloper:V3jUV7QXqEoAtnhy@cluster0-zhjde.mongodb.net";
+const client = new MongoClient(Db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+ 
+var _db;
+ 
+module.exports = {
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      // Verify we got a good "db" object
+      if (db)
+      {
+        _db = db.db("__CONCOX__");
+        console.log("Successfully connected to MongoDB."); 
+      }
+      return callback(err);
+         });
+  },
+ 
+  getDb: function () {
+    return _db;
+  },
+};
